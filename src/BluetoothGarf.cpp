929f059f8a8082c1a7ad67f1e6942b20272fc641
Trinque-hub <triv6014@usherbrooke.ca>
2025-11-24 10:56:05 -0500

    Fonction mis au propre

--- src/BluetoothGarf.cpp
+++ src/BluetoothGarf.cpp
@@ -3,11 +3,25 @@
 
 BluetoothSerial SerialBT;
 
-
+/*******************************************************************************************
+ * Auteur : Vincent
+ * 
+ * Envoie la position et l'état du robot sous forme de structure via Bluetooth
+ *
+ * @param Etat (structure) structure contenant l'état du robot
+ ******************************************************************************************/
 
 void envoieEtat(const etat &pos) { // Prend la reférence d'une structure Position en paramètre
   SerialBT.write((uint8_t*)&pos, sizeof(etat)); 
 }
+
+/*******************************************************************************************
+ * Auteur : Vincent
+ * 
+ * Lit la position et l'état du robot sous forme de structure via Bluetooth et la copie dans la structure passée en paramètre
+ *
+ * @param positionEtEtat (structure) structure contenant la position (x,y) et l'état du robot
+ ******************************************************************************************/
 
 bool recoieEtat(positionEtEtat &pos) {
   if (SerialBT.available() >= sizeof(positionEtEtat)) {
@@ -17,6 +31,11 @@ bool recoieEtat(positionEtEtat &pos) {
   return false;
 }
 
+/*******************************************************************************************
+ * Auteur : Vincent
+ * 
+ * Démarre le module Bluetooth en mode serveur
+ ******************************************************************************************/
 void setupBluetooth() // Initialisation du Bluetooth
 {
   SerialBT.begin("ESP32_Garfield"); // Nom du périphérique Bluetooth
